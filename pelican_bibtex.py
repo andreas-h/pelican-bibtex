7e2618daa332431d7a7b34ba54104cc2a19abd9f
Andreas Hilboll <andreas@hilboll.de>
2014-02-28 14:10:18 +0100

    ENH: support presentations and posters

--- pelican_bibtex.py
+++ pelican_bibtex.py
@@ -11,6 +11,8 @@ websites.
 # Author: Vlad Niculae <vlad@vene.ro>
 # Unlicense (see UNLICENSE for details)
 
+import os
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -33,9 +35,16 @@ def add_publications(generator):
     generator.context['publications']:
         List of tuples (key, year, text, bibtex, pdf, slides, poster).
         See Readme.md for more details.
+
     """
-    if 'PUBLICATIONS_SRC' not in generator.settings:
+    # check if settings are provided via pelicanconf.py
+    settings_present = False
+    for s in ['PUBLICATIONS_SRC','PRESENTATIONS_SRC', 'POSTERS_SRC']:
+        if s in generator.settings:
+            settings_present = True
+    if not settings_present:
         return
+
     try:
         from StringIO import StringIO
     except ImportError:
@@ -50,49 +59,75 @@ def add_publications(generator):
         logger.warn('`pelican_bibtex` failed to load dependency `pybtex`')
         return
 
-    refs_file = generator.settings['PUBLICATIONS_SRC']
-    try:
-        bibdata_all = Parser().parse_file(refs_file)
-    except PybtexError as e:
-        logger.warn('`pelican_bibtex` failed to parse file %s: %s' % (
-            refs_file,
-            str(e)))
-        return
+    for s, c in zip(['PUBLICATIONS_SRC','PRESENTATIONS_SRC', 'POSTERS_SRC'], 
+                    ['publications','presentations', 'posters']):
+        if s not in generator.settings:
+            continue
+        refs_file = generator.settings[s]
+        try:
+            bibdata_all = Parser().parse_file(refs_file)
+        except PybtexError as e:
+            logger.warn('`pelican_bibtex` failed to parse file %s: %s' % (
+                refs_file,
+                str(e)))
+            continue
 
-    publications = []
+        publications = []
 
-    # format entries
-    plain_style = plain.Style()
-    html_backend = html.Backend()
-    formatted_entries = plain_style.format_entries(bibdata_all.entries.values())
+        # format entries
+        plain_style = plain.Style()
+        html_backend = html.Backend()
+        formatted_entries = plain_style.format_entries(bibdata_all.entries.values())
 
-    for formatted_entry in formatted_entries:
-        key = formatted_entry.key
-        entry = bibdata_all.entries[key]
-        year = entry.fields.get('year')
-        pdf = entry.fields.pop('pdf', None)
-        slides = entry.fields.pop('slides', None)
-        poster = entry.fields.pop('poster', None)
+        for formatted_entry in formatted_entries:
+            key = formatted_entry.key
+            entry = bibdata_all.entries[key]
+            year = entry.fields.get('year')
+            slides = entry.fields.pop('slides', None)
+            poster = entry.fields.pop('poster', None)
+            
+            # add PDF link is file is present
+            # Zotero exports a 'file' field, which contains the 'Zotero' and
+            # 'Filesystem' filenames, seperated by ':'
+            filename = entry.fields['file'].split(':')[0]
+            if os.access(os.path.join('content', 'download', filename), os.R_OK):
+                pdf = os.path.join('download', filename)
+            else:
+                pdf = None
 
-        #render the bibtex string for the entry
-        bib_buf = StringIO()
-        bibdata_this = BibliographyData(entries={key: entry})
-        Writer().write_stream(bibdata_this, bib_buf)
-        text = formatted_entry.text.render(html_backend)
-        doi = entry.fields.get('doi') if 'doi' in entry.fields.keys() else ""
-        url = entry.fields.get('url') if 'url' in entry.fields.keys() else ""
+            #render the bibtex string for the entry
+            bib_buf = StringIO()
+            bibdata_this = BibliographyData(entries={key: entry})
+            Writer().write_stream(bibdata_this, bib_buf)
+            text = formatted_entry.text.render(html_backend)
+            doi = entry.fields.get('doi') if 'doi' in entry.fields.keys() else ""
+            url = entry.fields.get('url') if 'url' in entry.fields.keys() else ""
 
-        publications.append((key,
-                             year,
-                             text,
-                             bib_buf.getvalue(),
-                             doi,
-                             url,
-                             pdf,
-                             slides,
-                             poster))
+            # prettify entries
+            # remove BibTeX's {}
+            text = text.replace("\{", "")
+            text = text.replace("{", "")
+            text = text.replace("\}", "")
+            text = text.replace("}", "")
+            # subscript 2 in NO2, CO2, SO2
+            text = text.replace("NO2", "NO<sub>2</sub>")
+            text = text.replace("CO2", "CO<sub>2</sub>")
+            text = text.replace("CO2", "CO<sub>2</sub>")
+            # for posters and presentations, make for nicer printing
+            text = text.replace("In <em>", "Presented at <em>")
 
-    generator.context['publications'] = publications
+            publications.append((key,
+                                 year,
+                                 text,
+                                 bib_buf.getvalue(),
+                                 doi,
+                                 url,
+                                 pdf,
+                                 slides,
+                                 poster))
+
+            # store the list of artifacts in the generator context
+            generator.context[c] = publications
 
 
 def register():
